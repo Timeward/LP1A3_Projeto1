6ffa65b4249d2b34eb359d82e0c9deef64bd4147
Gh0st4v0 <89959988+Gh0st4v0@users.noreply.github.com>
2023-04-25 08:46:24 -0300

    Update Emprestimo.java

--- LP1A3_Projeto1/src/main/java/Emprestimo.java
+++ LP1A3_Projeto1/src/main/java/Emprestimo.java
@@ -1,13 +1,36 @@
 import java.util.Date;
 
 public class Emprestimo{
+    pripackage entities;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class Emprestimo {
+    private int id;
     private Livro livro;
     private Cliente cliente;
     private Usuario usuario;
     private Date dataEmprestimo;
     private Date dataDevolucao;
 
-    public Livro getLivro() {
+    public Emprestimo(int id, Livro livro, Cliente cliente, Usuario usuario, Date dataEmprestimo, Date dataDevolucao) {
+        this.id = id;
+        this.livro = livro;
+        this.cliente = cliente;
+        this.usuario = usuario;
+        this.dataEmprestimo = dataEmprestimo;
+        this.dataDevolucao = dataDevolucao;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+        public Livro getLivro() {
         return livro;
     }
 
@@ -46,4 +69,18 @@ public class Emprestimo{
     public void setDataDevolucao(Date dataDevolucao) {
         this.dataDevolucao = dataDevolucao;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Emprestimo that = (Emprestimo) o;
+        return id == that.id && Objects.equals(livro, that.livro) && Objects.equals(cliente, that.cliente) && Objects.equals(usuario, that.usuario) && Objects.equals(dataEmprestimo, that.dataEmprestimo) && Objects.equals(dataDevolucao, that.dataDevolucao);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, livro, cliente, usuario, dataEmprestimo, dataDevolucao);
+    }
 }
+
