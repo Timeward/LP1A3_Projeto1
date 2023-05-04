bcef2735fd620f5304ecf7b2e6578174cbfd4121
Timeward <sonicx1932@gmail.com>
2023-05-04 20:23:34 -0300

    Adicionei começo de certos métodos na Biblioteca.

--- LP1A3_Projeto1/src/main/java/Biblioteca.java
+++ LP1A3_Projeto1/src/main/java/Biblioteca.java
@@ -1,11 +1,20 @@
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Biblioteca {
     private List<Livro> livros;
     private List<Usuario> usuarios;
     private List<Emprestimo> emprestimos;
     
+    public Livro buscarLivro(){
+        //Inicio da função para encontrar o livro pelo pelo t[itulo, que não estava presente antes
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Está buscando por qual livro?");
+        String livroBuscado = scanner.nextLine();
+        return null;
+    }
+        
     public Livro retornaLivro(String nome) {
         // TODO: 24/04/2023
         return null;
@@ -17,16 +26,23 @@ public class Biblioteca {
 
     public Usuario buscarUsuarioNome(String nome) {
         // TODO: 24/04/2023
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o nome do usuário");
+        String usuarioBuscado = scanner.nextLine();
         return null;
     }
 
     public Usuario buscarUsuarioCpf(String cpf) {
         // TODO: 24/04/2023
-        return null;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o CPF do usuário");
+        String cpfBuscado = scanner.nextLine();
     }
 
     public void listarUsuarios() {
         // TODO: 24/04/2023
+        //Inicio da função listar usuarios usando a funçao forEach
+        usuarios.forEach(Usuario -> System.out.println(Usuario));
     }
 
     public void listarClientes() {
